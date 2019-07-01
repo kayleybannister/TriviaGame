417e2021050baa95a6519d581ae11265c73f7d22
Kayley Bannister <kayley.bannister@gmail.com>
2019-07-01 13:36:43 -0400

    trying to get a timer to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var correctCounter = 0;
 var incorrectCounter = 0;
 var intervalId;
 var clockRunning = false;
-var timeLeft = 0;
+var timeLeft = 10;
 
 var questions = [{
     question: "Who are the Duke and Duchess of Sussex?",
@@ -14,6 +14,11 @@ var questions = [{
     question: "What state is home to Clark Hill Lake?",
     choices: ["South Carolina", "Georgia", "Florida", "Tennessee"],
     correctAnswer: "Georgia"
+},
+{
+    question: "What state was Big Little Lies filmed in?",
+    choices: ["Georgia", "California", "Washington", "Orengon"],
+    correctAnswer: "California"
 },
 {
     question: "Who is the head football coach for the University of Georgia?",
@@ -31,9 +36,11 @@ var questions = [{
 //function for clicking Start - begins the entire game
 $("#start").on("click", function(){
 
+    $("#start-timer").append(timeLeft--);
+
     for (var i = 0; i < questions.length; i++) {
 
-        $("#start").append("<h3>" + questions[i].question + "<br><br>" + questions[i].choices + "</h3>");
+        $("#start").append("<h3>" + questions[i].question + "<BR><br>" + "CHOICES: <br>"+ questions[i].choices + "</h3>");
     };
 
 
